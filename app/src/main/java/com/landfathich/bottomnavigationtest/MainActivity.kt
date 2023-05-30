3eda430aed201bfc344b21800fe526ff16c15006
Arthur Kozhemyakin <trable228@icloud.com>
2023-05-30 21:47:14 +0300

    fragments done

--- app/src/main/java/com/landfathich/bottomnavigationtest/MainActivity.kt
+++ app/src/main/java/com/landfathich/bottomnavigationtest/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.landfathich.bottomnavigationtest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.landfathich.bottomnavigationtest.fragments.ExploreFragment
+import com.landfathich.bottomnavigationtest.fragments.HomeFragment
+import com.landfathich.bottomnavigationtest.fragments.LibraryFragment
+import com.landfathich.bottomnavigationtest.fragments.SubscriptionsFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
+        bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.navigation_explore -> {
+                    openFragment(ExploreFragment())
+                    return@setOnItemSelectedListener true
+                }
+                R.id.navigation_library -> {
+                    openFragment(LibraryFragment())
+                    return@setOnItemSelectedListener true
+                }
+                R.id.navigation_subscriptions -> {
+                    openFragment(SubscriptionsFragment())
+                    return@setOnItemSelectedListener true
+                }
+                R.id.navigation_home -> {
+                    openFragment(HomeFragment())
+                    return@setOnItemSelectedListener true
+                }
+
+                else -> return@setOnItemSelectedListener false
+            }
+        }
+    }
+
+    private fun openFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 }
